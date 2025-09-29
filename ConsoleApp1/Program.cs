4965abf79053fb987cf6e876cc08eb02cdfe1ed7
cherrynicole27 <fernandezcherrynicole05@gmail.com>
2025-09-29 21:36:40 +0800

    Initial commit: basic calculator with + and -

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Simple Calculator");
+
+        Console.Write("Enter first number: ");
+        int num1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Choose operation (+, -): ");
+        string op = Console.ReadLine();
+
+        int result = 0;
+
+        if (op == "+")
+            result = num1 + num2;
+        else if (op == "-")
+            result = num1 - num2;
+        else
+            Console.WriteLine("Invalid operator.");
+
+        Console.WriteLine("Result: " + result);
+    }
+}
